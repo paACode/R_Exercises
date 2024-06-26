d0047956ae47b543dd1ebb8713d3051caa19ae99
Pascal Ackermann <ackermann.pascal@gmail.con>
2024-06-26 09:02:53 +0200

    Exercise 7.1 completed

--- exercises.R
+++ exercises.R
@@ -1,2 +1,12 @@
 #IntroductionToRandRStudio_240502
 #Link to Document: https://drive.google.com/file/d/1w4fAjL3ByY1lsziR0vuKeYcb6ciHNF6J/view?usp=drive_link
+
+#Exercise 7.1
+temp1_fahrenheit  <- c(51.9, 51.8, 51.9, 53)
+temp1_degCelsius  <- (temp1_fahrenheit-32) * (5/9)
+
+temp2_fahrenheit  <- c(48, 48.2, 48, 48.7)
+temp2_deg_Celsius <- (temp2_fahrenheit-32) * (5/9)
+
+diff_temp <- temp1_fahrenheit -temp2_fahrenheit
+
