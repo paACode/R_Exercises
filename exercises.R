1e1c2d46fb790346a93f643660826b964cbace86
Pascal Ackermann <ackermann.pascal@gmail.con>
2024-06-26 11:08:35 +0200

    Added definition of Std Deviation of Sample

--- exercises.R
+++ exercises.R
@@ -2,7 +2,6 @@
 #Link to Document: https://drive.google.com/file/d/1w4fAjL3ByY1lsziR0vuKeYcb6ciHNF6J/view?usp=drive_link
 
 #Exercise 7.1
-
 # temp1_fahrenheit  <- c(51.9, 51.8, 51.9, 53)
 # temp1_degCelsius  <- (temp1_fahrenheit-32) * (5/9)
 # 
@@ -12,30 +11,48 @@
 # diff_temp <- temp1_fahrenheit -temp2_fahrenheit
 #-------------------------------------------------------------------------------
 #Exercise 7.2
-x <- c(4,2,1,3,3,5,7)
-third_entry <- x[3]
-fst_and_fth_entry <- x[c(1,4)]
-length(x)
-
-x+2 #Adds 2 to each element of vector x
-sum(x+2) # Adds up all the elements in vecotor (x+2)
-x <= 3 # Returns a vector with True or False statements 
-x[x <= 3] #outputs a vector with all element values <=3
-x[4] <- 8 #Replaces the 4th element with 8
-sort(x) # sorts elements by value 
-order(x) #Returns the indexes to element so that values are in order
-
-Name = c("Charlie","Anne", "Bert","Denise")
-Age = c(68,24,76,27)
-Status = c("old","young","retired","single")
-df = data.frame(Name,Age,Status)
-
-indexes_alphabetical = order(Name)
-df_alphabetical = data.frame(Name[indexes_alphabetical],
-                            Age[indexes_alphabetical], 
-                            Status[indexes_alphabetical])
-df_alphabetical_easy = df[order(Name),]
-df
-df_alphabetical
-df_alphabetical_easy
+# x <- c(4,2,1,3,3,5,7)
+# third_entry <- x[3]
+# fst_and_fth_entry <- x[c(1,4)]
+# length(x)
+# 
+# x+2 #Adds 2 to each element of vector x
+# sum(x+2) # Adds up all the elements in vecotor (x+2)
+# x <= 3 # Returns a vector with True or False statements 
+# x[x <= 3] #outputs a vector with all element values <=3
+# x[4] <- 8 #Replaces the 4th element with 8
+# sort(x) # sorts elements by value 
+# order(x) #Returns the indexes to element so that values are in order
+# 
+# Name = c("Charlie","Anne", "Bert","Denise")
+# Age = c(68,24,76,27)
+# Status = c("old","young","retired","single")
+# df = data.frame(Name,Age,Status)
+# 
+# indexes_alphabetical = order(Name)
+# df_alphabetical = data.frame(Name[indexes_alphabetical],
+#                             Age[indexes_alphabetical], 
+#                             Status[indexes_alphabetical])
+# df_alphabetical_easy = df[order(Name),]
+# df
+# df_alphabetical
+# df_alphabetical_easy
 #-------------------------------------------------------------------------------
+#Definition Standard Deviation and Variance
+
+weight <- c(60, 72, 57, 90, 95, 72)
+weight_mean <- mean(weight)
+
+diff_weight <- weight-weight_mean 
+
+std_deviation = sqrt((sum(diff_weight^2))/(length(weight)-1))
+round(std_deviation,5) == round(sd(weight),5)
+
+
+variance = sum(diff_weight^2)/(length(weight)-1)
+round(variance,5) == round(var(weight),5)
+#-------------------------------------------------------------------------------
+
+
+
+
